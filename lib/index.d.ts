98e98316dc13644228305ea53883af375ddce6b0
Scott Evers <severs@involta.com>
2021-04-07 22:53:05 -0500

    pre blowing up

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export class TimeSheetType {
-  constructor();
-  Monday: number;
-  Tuesday: number;
-
-  refreshFromSource(week: Date, email: string, provider: any): TimeSheetType
-}
-
-exports.TimeSheet
